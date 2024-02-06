97d93bdfac9d2a21cf366d3a88dad67acd18f0f9
Nathan <silentphoenix11@SilentPhoenix11.localdomain>
2024-02-05 20:27:53 -0500

    second commit

---
+++ Contact List/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import ContactList from './components/ContactList';
+
+function App() {
+  return (
+    <>
+      <ContactList />    
+    </>
+  );
+}
+
+export default App
